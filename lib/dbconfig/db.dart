375b810ea3e8b884752d275865ae1daf35d4a81a
alwaystaegi <ffff4565@naver.com>
2023-05-15 14:24:30 +0900

    frame change

---
+++ lib/dbconfig/db.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Database {
+
+
+  static final dburl = dotenv.env["MONGO_URL"].toString();
+
+
+  Future<BulkWriteResult> insertmany(String collection_name,
+      List<Map<String, dynamic>> value) async {
+    Db conn = await Db.create(dburl);
+    await conn.open();
+    DbCollection collection = conn.collection(collection_name);
+
+    var result = await collection.insertMany(value);
+    conn.close();
+    return result;
+  }
+
+  Future<Map<String, dynamic>> insert(String collectionName,
+      Map<String, dynamic> value) async {
+    Db conn = await Db.create(dburl);
+    await conn.open();
+    DbCollection collection = conn.collection(collectionName);
+
+    var result = await collection.insert(value);
+    conn.close();
+    return result;
+  }
+
+}
